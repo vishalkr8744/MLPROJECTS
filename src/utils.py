ee9c8786c55bc56b030a7bb42d5dc7d0470e1198
Vishal Kumar <vishalkr8744@gmail.com>
2026-03-11 02:06:00 +0530

    Deployment configeration for AWS

--- src/utils.py
+++ src/utils.py
@@ -42,6 +42,13 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
             report[list(models.keys())[i]]=test_model_score
 
         return report
-
     except Exception as e:
-        raise CustomException(e,sys)    
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+        try:
+            with open(file_path,"rb") as file_obj:
+                return dill.load(file_obj)
+
+        except Exception as e:
+         raise CustomException(e,sys)    
